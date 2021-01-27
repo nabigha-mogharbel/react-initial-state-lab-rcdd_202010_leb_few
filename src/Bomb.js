1e714f4d23f84155876ce22b25845c1c7ea4c9ff
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2021-01-27 21:06:49 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,13 @@
-// your Bomb code here!
+import React, { Component } from 'react'
+
+export default class Bomb extends Component {
+  constructor(props){
+    super()
+    this.state={secondsLeft: props.initialState}
+  }
+  render(){
+    return(
+      <div className="Bomb"></div>
+    )
+  }
+}
